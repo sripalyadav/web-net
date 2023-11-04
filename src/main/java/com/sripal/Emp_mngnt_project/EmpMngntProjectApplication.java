bf894195196b3efd602bd238e6d6452cc4533fe4
Sripalyadav <kookatlasripalyadav@gmail.com>
2023-11-04 15:56:38 +0530

    This is first commit

---
+++ src/main/java/com/sripal/Emp_mngnt_project/EmpMngntProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sripal.Emp_mngnt_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMngntProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpMngntProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sripal/Emp_mngnt_project/Entity/Employee.java
@@ -0,0 +1,46 @@
+package com.sripal.Emp_mngnt_project.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "EMDT")
+@Data
+public class Employee {
+	
+
+	@Id
+	@Column(name = "EMployee_Id")
+	private int employeeid;
+	
+	@Column(name = "Employee_Name")
+	private String employeename;
+	
+	@Column(name = "Employee_age")
+	private String age;
+	
+	@Column(name = "Date Of Birth")
+	private String dob;
+	
+	@Column(name = "Gender")
+	private String gender;
+	
+	@Column(name = "Email")
+	private String email;
+	
+	@Column(name = "Password")
+	private String password;
+	
+	@Column(name = "Address")
+	private String address;
+	
+	@Column(name = "Mobile")
+	private long mobile;
+	
+	
+	
+	
+}
